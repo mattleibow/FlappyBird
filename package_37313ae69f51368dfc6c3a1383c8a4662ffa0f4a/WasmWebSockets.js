0367e3147b9ad42ea558b30c007e7138da5244ac
Matthew Leibowitz <mattleibow@live.com>
2016-08-30 04:30:05 +0200

    Add the initial Debug version of the game

---
+++ package_37313ae69f51368dfc6c3a1383c8a4662ffa0f4a/WasmWebSockets.js
@@ -0,0 +1,106 @@
+﻿define([""], () => {
+});
+
+class ActiveSocket {
+    constructor(handle, socket) {
+        this.handle = handle;
+        this.socket = socket;
+    }
+}
+
+var WebSocketInterop = {
+
+    activeSockets: {},
+    debug: false,
+
+    connect: function (handle, url) {
+        this.ensureInitialized();
+
+        if (this.debug) console.log("WebSocketInterop: connect " + url);
+
+        var webSocket = new WebSocket(url);
+        webSocket.binaryType = "arraybuffer";
+
+        webSocket.onopen = function () {
+            if (this.debug) console.log(`Socket is opened [${webSocket.protocol}] ${WebSocketInterop.dispatchConnectedMethod}`);
+
+            WebSocketInterop.dispatchConnectedMethod(String(handle), webSocket.protocol);
+        };
+
+        webSocket.onerror = function (evt) {
+            WebSocketInterop.dispatchErrorMethod(String(handle), String(evt.error));
+        };
+
+        webSocket.onclose = function (evt) {
+            WebSocketInterop.dispatchClosedMethod(String(handle), webSocket.readyState);
+        };
+
+        webSocket.onmessage = function (evt) {
+            var msg = evt.data;
+
+            if (msg instanceof ArrayBuffer) {
+                if (this.debug) console.log(`Received ArrayBuffer`);
+
+                if (msg !== null) {
+                    var arraySize = msg.byteLength;
+
+                    if (this.debug) console.log(`Result: ${msg} / ${arraySize}`);
+
+                    var ptr = Module._malloc(arraySize);
+                    try {
+                        writeArrayToMemory(new Int8Array(msg), ptr);
+
+                        WebSocketInterop.dispatchReceivedBinaryMethod(String(handle), ptr, arraySize);
+                    }
+                    finally {
+                        Module._free(ptr);
+                    }
+                }
+                else {
+                    if (this.debug) console.error(`empty arraybuffer ? ${msg}`);
+                }
+            }
+            else {
+                if (this.debug) console.log(`Received message ${msg}`);
+            }
+        };
+
+        this.activeSockets[handle] = new ActiveSocket(handle, webSocket);
+    },
+
+    close: function (handle, code, statusDescription) {
+        this.getActiveSocket(handle).close(code, statusDescription);
+
+        delete this.activeSockets[handle];
+    },
+
+    send: function (handle, pData, count, offset) {
+        var data = new ArrayBuffer(count);
+        var bytes = new Int8Array(data);
+
+        for (var i = 0; i < count; i++) {
+            bytes[i] = Module.HEAPU8[pData + i + offset];
+        }
+
+        this.activeSockets[handle].socket.send(data);
+    },
+
+    getActiveSocket: function (handle) {
+
+        var activeSocket = this.activeSockets[handle];
+
+        if (activeSocket === null) {
+            throw `Unknown WasmWebSocket instance ${handle}`;
+        }
+
+        return activeSocket.socket;
+    },
+
+    ensureInitialized: function () {
+
+        WebSocketInterop.dispatchConnectedMethod = Module.mono_bind_static_method("[Uno.Wasm.WebSockets] Uno.Wasm.WebSockets.WasmWebSocket:DispatchConnected");
+        WebSocketInterop.dispatchErrorMethod = Module.mono_bind_static_method("[Uno.Wasm.WebSockets] Uno.Wasm.WebSockets.WasmWebSocket:DispatchError");
+        WebSocketInterop.dispatchReceivedBinaryMethod = Module.mono_bind_static_method("[Uno.Wasm.WebSockets] Uno.Wasm.WebSockets.WasmWebSocket:DispatchReceivedBinary");
+        WebSocketInterop.dispatchClosedMethod = Module.mono_bind_static_method("[Uno.Wasm.WebSockets] Uno.Wasm.WebSockets.WasmWebSocket:DispatchClosed");
+    }
+};
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+console.debug("[ServiceWorker] Initializing");
+
+let config = {};
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        fetch("./package_37313ae69f51368dfc6c3a1383c8a4662ffa0f4a/uno-config.js")
+            .then(r => r.text()
+                .then(configStr => {
+                    eval(configStr);
+                    caches.open('package_37313ae69f51368dfc6c3a1383c8a4662ffa0f4a').then(function (cache) {
+                        console.debug('[ServiceWorker] Caching app binaries and content');
+                        return cache.addAll(config.offline_files);
+                    });
+                }
+                )
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed
